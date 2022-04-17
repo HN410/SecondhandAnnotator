c6fa916362f6c39ddd46a87760e548a50f13d16e
HN410 <HN410>
2022-04-17 14:45:53 +0900

    add TogglePane

---
+++ src/components/TogglePane.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+        <v-btn-toggle v-model="toggleSelected" mandatory>
+            <v-btn>ジャンル</v-btn>
+            <v-btn>商品種別</v-btn>
+            <v-btn>数量</v-btn>
+            <v-btn>そのほか</v-btn>
+        </v-btn-toggle>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'TogglePane',
+
+    data: () => ({
+        "toggleSelected": undefined,
+      
+    }),
+    props: {
+        "which": Number
+    },
+  }
+</script>
