36bc6efdadb84ae6e847139779dc091c492ef67c
HN410 <HN410>
2022-04-17 14:45:46 +0900

    add TextPane

--- src/components/MainPane.vue
+++ src/components/MainPane.vue
@@ -1,17 +1,30 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      
+      <v-col>
+        <TextPane v-bind:val="textData"></TextPane>
+      </v-col>
+      <v-col>
+        <TogglePane v-bind:which="tagSelected"></TogglePane>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+  import TextPane from './TextPane';
+  import TogglePane from './TogglePane';
   export default {
     name: 'MainPane',
 
     data: () => ({
-      
+            textData: "test", 
+            tagSelected: 1
     }),
+
+    components: {
+      TextPane,
+      TogglePane
+    },
   }
 </script>
---
+++ src/components/TextPane.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+        <v-btn>{{val}}</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'TextPane',
+
+    data: () => ({
+      
+    }),
+    props: ["val"],
+  }
+</script>
