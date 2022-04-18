48de777e10f10dac4ec0f351acc1fb009b53045e
HN410 <HN410>
2022-04-18 22:48:16 +0900

    add arrayEqual

--- src/components/Common.js
+++ src/components/Common.js
@@ -3,6 +3,15 @@ const COLORS = ["blue", "green", "brown", "red"];
 const COLOR_DEFAULT = "grey lighten-5";
 const TAG_LIST = ["ジャンル [a]", "商品種別 [s]", "数量 [d]", "そのほか [f]"];
 
+function arrayEqual(a, b) {
+    if (!Array.isArray(a))    return false;
+    if (!Array.isArray(b))    return false;
+    if (a.length != b.length) return false;
+    for (var i = 0, n = a.length; i < n; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
 export default{
-    COLOR_LIGHTEN, COLOR_DEFAULT, COLORS, TAG_LIST
+    COLOR_LIGHTEN, COLOR_DEFAULT, COLORS, TAG_LIST, arrayEqual
 }
